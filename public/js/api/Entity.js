f8f8371dd618ea97d07cdf38dcf626b0324bf4a2
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-18 17:42:26 +0300

    made Entity without verify

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -10,7 +10,20 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static list( data, callback = f => f ) {
-
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', `${Entity.URL}`);
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === 4 && xhr.status === 200){
+        try {
+          let data = JSON.parse(xhr.response);
+          return data;
+        } catch (e) {
+          callback(`Произошла ошибка ${data}`);
+        }
+      } 
+    })
+    xhr.send(data);
   }
 
   /**
@@ -19,7 +32,22 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create( data, callback = f => f ) {
-
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let dataCopy = Object.assign(data); // скопировал ради иммутабильности
+    dataCopy._method = "PUT";
+    xhr.open('POST', `${Entity.URL}`);
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === 4 && xhr.status === 200){
+        try {
+          let data = JSON.parse(xhr.response);
+          return data;
+        } catch (e) {
+          callback(`Произошла ошибка ${data}`);
+        }
+      } 
+    })
+    xhr.send(dataCopy);
   }
 
   /**
@@ -27,7 +55,10 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static get( id = '', data, callback = f => f ) {
-
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', `${Entity.URL}`);
+    //  что делать с id нужно? Как с ним работать?
   }
 
   /**
@@ -35,7 +66,23 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove( id = '', data, callback = f => f ) {
-
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let dataCopy = Object.assign(data);
+    dataCopy._method = "DELETE";
+    dataCopy.id = 21;
+    xhr.open('POST', `${Entity.URL}`);
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === 4 && xhr.status === 200){
+        try {
+          let data = JSON.parse(xhr.response);
+          return data;
+        } catch (e) {
+          callback(`Произошла ошибка ${data}`);
+        }
+      } 
+    })
+    xhr.send(dataCopy);
   }
 }
 
